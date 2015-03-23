831a769602719311fde029f4ca553b46d5733513
Andriy Khavryuchenko <akhavr@khavr.com>
2015-03-23 17:52:08 +0200

    This time will release just source pkg

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_long_description():
 
 setup(
     name="42cc-pystyle",
-    version="0.0.7",
+    version="0.0.8",
     description="flake8 checks for 42 Coffee Cups style guide",
     long_description=get_long_description(),
     license=open('LICENSE').read(),
